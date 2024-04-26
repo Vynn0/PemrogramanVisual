73a12430dbe29be8bc6328a9ce57b26e99797370
Vynn <121730359+Vynn0@users.noreply.github.com>
2024-04-26 13:15:14 +0700

    Add files via upload

---
+++ M09/main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+Frameku = tk.Frame(root, bg = 'blue')
+Frameku.place(relwidth = 0.8, relheight = 0.8)
+
+Tombolku = tk.Button(Frameku, text = "Tes Tombol", bg = 'gray', fg = 'red')
+Tombolku.pack()
+
+Entryku = tk.Entry(Frameku, bg = 'green')
+Entryku.pack()
+
+root.mainloop()
